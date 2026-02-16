170f5737a149d02a2ea187cbe1679c82eba32152
DovgaNik <nikita.dovgan.05@hotmail.com>
2026-02-16 15:41:44 +0100

    Fixed a problem with background being over the picture.

--- app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -31,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheGreatestCocktailAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GradientBackground {
+                    GradientBackground(modifier = Modifier.padding(innerPadding)) {
                         CocktailImage()
                     }
                 }
@@ -41,23 +43,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GradientBackground(content: @Composable () -> Unit) {
+fun GradientBackground(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
     Box(
-        Modifier
+        modifier
             .background(
                 brush = Brush.verticalGradient(
                     colors = listOf(
-
                         colorResource(R.color.teal_700),
                         colorResource(R.color.purple_700)
-
                     )
                 )
             )
-            .fillMaxSize()
+            .fillMaxSize(),
     ) {
-        Unit
-
+        content()
     }
 }
 
