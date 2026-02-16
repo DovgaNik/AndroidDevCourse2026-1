f7f35ca36fc92c442c21579d78d5ae9e66328e4c
DovgaNik <nikita.dovgan.05@hotmail.com>
2026-02-16 15:32:28 +0100

    Added background.

--- app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -16,8 +16,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import dev.dovhan.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
@@ -29,10 +31,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheGreatestCocktailAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CocktailImage()
+                    GradientBackground {
+                        CocktailImage()
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun GradientBackground(content: @Composable () -> Unit) {
+    Box(
+        Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+
+                        colorResource(R.color.teal_700),
+                        colorResource(R.color.purple_700)
+
+                    )
+                )
+            )
+            .fillMaxSize()
+    ) {
+        Unit
+
     }
 }
 
