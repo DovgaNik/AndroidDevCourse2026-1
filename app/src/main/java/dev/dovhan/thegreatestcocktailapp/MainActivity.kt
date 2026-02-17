5ffdfb142ec08c84933e0ced5d45ef951777bfc5
DovgaNik <nikita.dovgan.05@hotmail.com>
2026-02-17 14:08:12 +0100

    Added navbar

--- app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/dev/dovhan/thegreatestcocktailapp/MainActivity.kt
@@ -1,21 +1,21 @@
 package dev.dovhan.thegreatestcocktailapp
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import android.widget.Toast.makeText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -31,28 +31,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             val context = LocalContext.current
             TheGreatestCocktailAppTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopAppBar(
-                            title = { Text("Cocktail App") },
-                            actions = {
-                                IconButton(
-                                    {
-                                        makeText(context, "Cocktail added to favs", 3).show()
-                                    }
-                                ) {
-                                    Icon(
-                                        painter = painterResource(R.drawable.heart_icon),
-                                        contentDescription = "Favorite Icon",
-                                        tint = Color.Unspecified,
-                                        modifier = Modifier.padding(4.dp)
-                                    )
-                                }
-                            }
-                        )
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    TopAppBar(title = { Text("Cocktail App") }, actions = {
+                        IconButton(
+                            {
+                                makeText(context, "Cocktail added to favs", 3).show()
+                            }) {
+                            Icon(
+                                painter = painterResource(R.drawable.heart_icon),
+                                contentDescription = "Favorite Icon",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.padding(4.dp)
+                            )
+                        }
+                    })
+                }, bottomBar = {
+                    NavigationBar() {
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {},
+                            label = { Text("Cocktails") },
+                            icon = {
+                                Icon(
+                                    painterResource(R.drawable.cocktail_icon),
+                                    contentDescription = "cocktail icon",
+                                    modifier = Modifier.size(32.dp)
+                                )
+                            })
+
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {},
+                            label = { Text("Categories") },
+                            icon = {
+                                Icon(
+                                    painterResource(R.drawable.category),
+                                    contentDescription = "cocktail icon",
+                                    modifier = Modifier.size(32.dp)
+                                )
+                            })
                     }
-                ) { innerPadding ->
+                }) { innerPadding ->
                     DetailedCocktail(innerPadding)
                 }
             }
